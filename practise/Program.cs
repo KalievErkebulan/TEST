d0e1f0b7e746a11fc9e517ad5b705add0a10ab36
Erkebulan <kaliev.e.m@gmail.com>
2023-01-17 01:15:58 +0600

    sqr random number

---
+++ practise/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+int sqr = number*number;
+
+Console.WriteLine("Квадрат числа равен: " + sqr);  
+Console.WriteLine($"Квадрат числа {number} равен: {sqr}");
+double sqr2 = Math.Pow(number,2);
+Console.WriteLine($"Квадрат числа {number} равен: {sqr}");
---
+++ test6/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+int f1 = 1;
+int f2 = 2;
+int dg = 5;
+int f; 
+int count = 0;
+int di = 10;
+
+while(di > 1)
+{
+    int T;
+    if(f1 == 1 ){
+       T = di/(f1+dg);
+    } 
+    
+    if(f2 == 2){
+        T = di/(f2+dg);
+    }
+    count++;
+}
