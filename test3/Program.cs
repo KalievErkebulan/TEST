a0e028b85549168451f0df4ec7e34ae9ee9bc09e
Erkebulan <kaliev.e.m@gmail.com>
2023-01-16 20:16:45 +0600

    max value

---
+++ test3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("write your name : ");
+string name = Console.ReadLine();
+if (name.ToLower() =="tilek"){
+    Console.WriteLine("Haliluja he is Tilek ");
+}else if (name =="Flek"){
+        Console.WriteLine("Hello,"+ name );
+}
+else{
+    Console.WriteLine("POshel nakhui " + name + "!");
+}
+
