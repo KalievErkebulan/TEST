51378fca8a8b2611c0e038ab04a3878d4f8c5874
Erkebulan <kaliev.e.m@gmail.com>
2023-01-17 21:02:44 +0600

    min max value

---
+++ homework/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число:");
+double numberA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число:");
+double numberB = int.Parse(Console.ReadLine()!);
+
+if
+    (numberA>numberB)
+    {
+        Console.Write($"max = {numberA}");
+        Console.WriteLine($" min = {numberB}");
+    }
+else if (numberA<numberB)
+{
+        Console.Write($"max = {numberB}");
+        Console.WriteLine($" min = {numberA}");
+}
+else{
+        Console.Write($"min = {numberB}");
+}
+
