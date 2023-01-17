c626c530cafe8ccb90f0fccf02fd0d951579be83
Erkebulan <kaliev.e.m@gmail.com>
2023-01-17 19:38:12 +0600

    day on week

---
+++ practise3/Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.Clear();
+System.Console.WriteLine("Введите номер:");
+int number = int.Parse(Console.ReadLine()!);
+switch (number)
+{
+    case 5:
+    Console.WriteLine("Сегодня пятница");
+    break;
+    case 2:
+    Console.WriteLine("Сегодня вторник");
+    break;
+    case 3:
+    Console.WriteLine("Сегодня среда");
+    break;
+    default:
+    Console.WriteLine("Это другой день");
+    break;
+}
