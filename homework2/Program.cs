c94f1e1668da5a93e0e701f1add98ab1daaf13cb
Erkebulan <kaliev.e.m@gmail.com>
2023-01-17 22:07:35 +0600

    max of 3

---
+++ homework2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+System.Console.WriteLine("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите второе число ");
+int numberB = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите третье число ");
+int numberC = int.Parse(Console.ReadLine()!);
+
+if  (numberA > numberB && numberA > numberC)
+    
+    Console.WriteLine($"max value equal to {numberA}");
+
+else if (numberB > numberA && numberB > numberC) 
+
+    Console.WriteLine($"max value equal to {numberB}");
+
+else if (numberC > numberA && numberC > numberB) 
+
+    Console.WriteLine($"max value equal to {numberC}");
+
+
