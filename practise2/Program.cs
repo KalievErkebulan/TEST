895138a2dfea8b340ae1d413d341556acb2a3873
Erkebulan <kaliev.e.m@gmail.com>
2023-01-17 16:54:00 +0600

    numberA sqr eaqul to numberB

---
+++ practise2/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Clear();
+Console.WriteLine("Введите первое число:");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число:");
+int numberB = int.Parse(Console.ReadLine()!);
+if(numberB ==numberA*numberA){
+    System.Console.WriteLine("yes");
+}
+else{
+    System.Console.WriteLine("no");
+}
