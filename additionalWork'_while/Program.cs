5ef073046799e8df77cebcc15b6a497f20b7c9d1
Erkebulan <kaliev.e.m@gmail.com>
2023-01-19 01:08:04 +0600

    bank deposit with accrued 7 percent method while

---
+++ additionalWork'_while/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+double depositAmount = double.Parse(Console.ReadLine()!);
+double month = double.Parse(Console.ReadLine()!);
+double percent = 0.07D;
+double percentForMonth = percent*depositAmount;
+double finalDepositAmount = 0;
+double oneMonth = 1;
+while(oneMonth<=month) 
+{
+  if (finalDepositAmount==0 &&finalDepositAmount<depositAmount)
+        {
+            if(depositAmount!=0 && percentForMonth>0)
+                {
+                    finalDepositAmount = percentForMonth*month;
+                }
+        }
+                        oneMonth++;
+                        Console.WriteLine($"За 1 месяц вы получите проценты {percentForMonth}");
+}
+                            Console.WriteLine($"Итого вы получите {finalDepositAmount}");
