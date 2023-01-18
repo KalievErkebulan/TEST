76caa5799bf4d2a4b79655d814a148296c40176a
Erkebulan <kaliev.e.m@gmail.com>
2023-01-18 18:27:28 +0600

    even odd values of number

---
+++ homework3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+float number = float.Parse(Console.ReadLine()!);
+if (number%2==0 )
+{
+    Console.WriteLine("yes");
+}
+else if (number%2==1)
+{
+    Console.WriteLine("no");
+}
