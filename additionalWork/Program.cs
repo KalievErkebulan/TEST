ef776d3014d962b93c376dbbeed852f5b68deac8
Erkebulan <kaliev.e.m@gmail.com>
2023-01-19 00:36:40 +0600

    bank deposit with accrued 7 percent

---
+++ additionalWork/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+double depositAmount = double.Parse(Console.ReadLine()!);
+double month = double.Parse(Console.ReadLine()!);
+double percent = 0.07D;
+double percentForMonth = percent*depositAmount;
+double finalDepositAmount=0;
+for (double oneMonth = 1; oneMonth<=month; oneMonth++)
+{
+    if (finalDepositAmount==0 &&finalDepositAmount<depositAmount)
+        {
+            if(depositAmount!=0 && percentForMonth>0)
+                {
+                    finalDepositAmount = percentForMonth*month;
+                    Console.WriteLine(finalDepositAmount);
+                }
+        }
+                    Console.WriteLine($"{oneMonth} месяца и их проценты {percentForMonth}");
+}
