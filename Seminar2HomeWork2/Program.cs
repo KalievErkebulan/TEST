5a9dd0e54f6eca75151515bd3bb0ee513cbbea8f
Erkebulan <kaliev.e.m@gmail.com>
2023-01-20 02:48:42 +0600

    Seminar2HomeWork2

---
+++ Seminar2HomeWork2/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Напишите программу,которая выводит случайное трёхзначное число
+и удаляет вторую цифру этого числа.
+456-->46
+782-->72
+918-->98*/
+Console.Clear();
+int randomThreeDigitNumber = new Random().Next(100,1000);
+int remainder = randomThreeDigitNumber%10;
+int division = randomThreeDigitNumber/100;
+int multiplication = division*10;
+int addition = multiplication+remainder;
+Console.WriteLine(randomThreeDigitNumber);
+Console.WriteLine(addition);
