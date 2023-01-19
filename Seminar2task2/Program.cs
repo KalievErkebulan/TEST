d964e9a88c2d4ed33ab8516f6a7a77cfdec43fed
Erkebulan <kaliev.e.m@gmail.com>
2023-01-20 01:03:04 +0600

    2ndSeminarTask2

---
+++ Seminar2task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+int number = new Random().Next(10,100);
+int a = number%10;
+int b = number/10;
+System.Console.WriteLine(a);
+System.Console.WriteLine(b);
+int max=a;
+if(max<b) max=b;
+{
+    System.Console.WriteLine(max);
+}
