e63d56ebcd23de00a3dd1e72d91b0e7c86fe7c10
Erkebulan <kaliev.e.m@gmail.com>
2023-01-20 01:33:44 +0600

    Seminar2Task3

---
+++ Seminar2Task3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+int numberOne = int.Parse(Console.ReadLine()!);
+int numberTwo = int.Parse(Console.ReadLine()!);
+float resultDividesNumbers = numberTwo%numberOne;
+if(resultDividesNumbers==0)
+    Console.WriteLine("Число кратно: " );
+else 
+    Console.WriteLine("Число не кратно: " + resultDividesNumbers);
