2a21dac7c569a5a5c7b1ebd3316716c17f8ef3eb
Erkebulan <kaliev.e.m@gmail.com>
2023-01-20 00:33:35 +0600

    2ndSeminarTask1

---
+++ TilekHomeWork/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Введите дату рождения (день - месяц - год): ");
+DateTime birthDate = DateTime.Parse(Console.ReadLine()!);
+DateTimeKind kind;
+for(DateTime now = DateTime.Now; birthDate!=now;)
+    {
+        if (now!=birthDate)
+            {
+                Console.WriteLine("С днём рождения!!!");
+                break;
+            }
+                else if (now!=birthDate)
+                    {
+                        Console.WriteLine("Today another day!");
+                        break;
+                    }
+    }
+//.ToString("dd.MM");
---
+++ seminar2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int number = int.Parse(Console.ReadLine()!);
+while(number!=0)
+{
+    if(number>0)
+    {
+    Console.WriteLine(number%10);
+    }
+break;
+}
