7dfb4b2c795a54f7d0240a5c30d0c6dc5cb91782
Erkebulan <kaliev.e.m@gmail.com>
2023-01-21 04:35:43 +0600

    Seminar2HomeWork3

---
+++ Seminar2HomeWork3/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Напишите программу,которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+645-->5
+78-->третьей цифры нет
+32679-->6
+*/
+Console.Clear();
+int number = int.Parse(Console.ReadLine()!);
+int exampleOfThreeDigitNumber = 345;
+string strTwo = $"{exampleOfThreeDigitNumber}";
+string strOne = $"{number}";
+int lengthOne = strOne.Length;
+int lengthTwo = strTwo.Length;
+int lengthDifference =lengthOne-lengthTwo;
+int degree = Convert.ToInt32(Math.Pow(10,lengthDifference));
+    if( lengthOne < lengthTwo)
+    {
+                        Console.WriteLine($"{number} Третьей цифры нет");
+    }
+    else if (lengthOne>lengthTwo)
+    {
+        do
+        {
+            number = (number /degree );
+            break;
+        }
+        while (lengthTwo<=lengthOne);
+        {    
+            number = number % 10;
+            Console.WriteLine($"The third digit of this number is {number}");
+        }
+    }
+    else
+    {
+        number = number % 10; 
+        System.Console.WriteLine($"The third digit of this number is {number}");
+    }
