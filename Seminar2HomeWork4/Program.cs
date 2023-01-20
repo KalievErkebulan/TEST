cb528a7be61d09ce0450842d27dbb9fce7b6066a
Erkebulan <kaliev.e.m@gmail.com>
2023-01-21 04:56:56 +0600

    Seminar2HomeWork4

---
+++ Seminar2HomeWork4/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным*/
+Console.Clear();
+Console.WriteLine("Введите свое число: ");
+int number = int.Parse(Console.ReadLine()!);
+switch (number)
+{   
+    case 7:
+    Console.WriteLine("да");
+    break;
+    case 6:
+    Console.WriteLine("да");
+    break;
+    default:
+    Console.WriteLine("нет");
+    break;
+}
